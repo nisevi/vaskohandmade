18cc54688f2c7efb4af7645142d05836936637d2
nisevi <nicolas.s.vidal@gmail.com>
2017-12-12 14:08:32 +0100

    It will be an static website by now.

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('vaskohandmade', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
