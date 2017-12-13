219c2c39b52888a2aa3de9970cc0afb249a25e20
nisevi <nicolas.s.vidal@gmail.com>
2017-12-13 01:19:09 +0100

    Removed circle functionality.

--- assets/custom/js/script.js
+++ assets/custom/js/script.js
@@ -306,47 +306,6 @@
             });
         })();
 
-        /* Circle Progress */
-        (function () {
-            function animateElements() {
-                $('.progressbar').each(function () {
-                    var elementPos = $(this).offset().top;
-                    var topOfWindow = $(window).scrollTop();
-                    var percent = $(this).find('.circle').attr('data-percent');
-                    var percentage = parseInt(percent, 10) / parseInt(100, 10);
-                    var animate = $(this).data('animate');
-                    var preAnimate = $(this).data('pre-animate');
-                    if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
-                        $(this).data('animate', true);
-                        $(this).find('.circle').circleProgress({
-                            startAngle: -Math.PI / 2,
-                            value: percent / 100,
-                            thickness: 3,
-                            fill: {
-                                color: '#E84855'
-                            }
-                        }).on('circle-animation-progress', function (event, progress, stepValue) {
-                            $(this).find('div').text((stepValue * 100).toFixed(1) + "%");
-                        }).stop();
-                    } else if (!preAnimate && !animate) {
-                        $(this).data('pre-animate', true);
-                        $(this).find('.circle').circleProgress({
-                            startAngle: -Math.PI / 2,
-                            value: 0,
-                            thickness: 3,
-                            fill: {
-                                color: '#E84855'
-                            }
-                        });
-                    }
-                });
-            }
-
-            // Show animated elements
-            animateElements();
-            $(window).scroll(animateElements);
-        })();
-
     });
 
     // Soft scroll
